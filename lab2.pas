24d61dbe251f6cdbe9a86bae0d3adcbc5e0bb04e
Oleksandr Markov <ol.markov1999@gmail.com>
2023-09-19 06:10:15 +0300

    Розгалужені структури: if...else

---
+++ lab2.pas
@@ -0,0 +1,30 @@
+﻿var 
+  a,b,c : real;
+   
+begin
+  writeln('Лабораторна робота №2 (варіант №2)');
+  (*Введення даних *) 
+  write('a: ');
+  read(a);
+  write('b: ');
+  read(b);
+  write('c: ');
+  read(c);
+  
+  if (a >= b) and (b >= c) then
+  begin
+    a := 2*a;
+    b := 2*b;
+    c := 2*c;
+  end
+  else
+  begin
+    a := abs(a);
+    b := abs(b);
+    c := abs(c);    
+  end;
+      
+  writeln('A: ', a);
+  writeln('B: ', b);
+  writeln('C: ', c);  
+end.
