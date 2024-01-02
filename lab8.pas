1a75b731dcd8f4886c3252e86b613b7543e8349c
Oleksandr Markov <ol.markov1999@gmail.com>
2024-01-02 23:25:18 +0200

    8: Робота з множинами

---
+++ lab8.pas
@@ -0,0 +1,25 @@
+﻿{
+Дано:  type  M=set  of  0..99;
+        A=set of byte;
+Обчислити кількість елементів в множині A типу M.
+}
+
+type  M=set  of  0..99;
+A = set of byte;
+
+var 
+  setA : A;
+  res : byte;
+ 
+begin
+// Заповнення множини довільними значеннями типу byte (0..255)
+for var i :=0 to 255 do
+  include(setA, random(0,255));
+ 
+ foreach var i in setA do
+   begin
+     if i < 100 then inc(res);
+   end;
+   
+  writeln('Елементів типу M: ', res);
+end.
