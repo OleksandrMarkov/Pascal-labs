c0b07ddcb22ad4cea2fad40eb7c02e3f9469b10d
Oleksandr Markov <ol.markov1999@gmail.com>
2024-01-03 21:11:05 +0200

    9: Робота з txt файлами

---
+++ lab9.pas
@@ -0,0 +1,35 @@
+﻿{
+Дано файл f, компоненти якого є цілими числами.
+Отримати в файлі g всі компоненти файла f,
+які діляться на 3 і не діляться на 7.
+}
+
+var 
+  f, g : text; // файли
+  s : string;  // рядок
+  i : integer; // ітератор
+  numbers: array of string; // числа в рядку
+  
+  begin
+    // відкриття файлів на читання / запис відповідно
+    assign(f, 'f.txt');
+    reset(f);
+    assign(g, 'g.txt');
+    rewrite(g);
+    
+    i := 0;
+    while not EOF(f) do
+    begin
+      readln(f,s);
+      numbers:= s.Split(); // розбиття рядку на масив
+      s := '';
+      for i := 0 to numbers.Length - 1 do
+        if ( strToInt(numbers[i]) mod 3 = 0 ) and
+          ( strToInt(numbers[i]) mod 7 <> 0 ) then
+            s := s + numbers[i] + ' ';
+      writeln(g,s); // запис
+    end;
+    // закриття файлів
+    close(g);
+    close(f);
+  end.
